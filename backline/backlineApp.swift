bce48f10f9a37e12ea5f8df5ebd6ab6f059c6fb3
Khadija Aslam <khadijaaslam2000@gmail.com>
2026-03-16 14:37:01 -0400

    Initial Commit

---
+++ backline/backlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  backlineApp.swift
+//  backline
+//
+//  Created by Khadija Aslam on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct backlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
